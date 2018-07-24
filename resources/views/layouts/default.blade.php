ff071b77432055908aec8f05aa12bbc12654877b
simpigo <simpigo@sina.com>
2018-07-24 13:57:25 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,15 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Laravel') -- Lezhi study</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+    @include('layouts._header')
+    <div class="container">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/about', 'StaticPagesController@about');
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+
+Route::get('/signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
