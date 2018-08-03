49a09a6fae4055dd136c7bd09835902d9a06a892
simpigo <simpigo@sina.com>
2018-08-03 05:32:34 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,8 +27,14 @@ class SessionsController extends Controller
         ]);
 
         if(Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '登录成功');
-            return redirect()->intended(route('users.show', [Auth::user()]));
+            if(Auth::user()->activated) {
+                session()->flash('success', '登录成功');
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            } else {
+                Auth::logout();
+                session()->flash('warning', '您的账号未激活，请检查邮箱中的注册邮件激活账号');
+                return redirect('/');
+            }
         } else {
             session()->flash('danger', '很抱歉，您的邮箱或密码不正确');
             return redirect()->back();
